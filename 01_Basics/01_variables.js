3ebb7902444759cce16af74ac85a2dd66e7874e9
Hassanzia88 <hzia2346@gmail.com>
2025-07-31 13:36:21 +0000

    conatiner config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 12345
+// varaiables methods
+let accountEmail = "hzia3173@gmail.com"
+var accocuntPassword = "212121"
+accountCity = "Bahawalpur"
+
+console.log(accountId)
+
+// verfications
+
+accountEmail= "hzia2346@gmail.com"
+accountPassword = "343434"
+accounutCity = "Lahore"
+
+// sb ko combine krky console krna h to
+
+console.table([accountEmail, accountPassword, accountCity])
+
+// prefer to not use var
+// because of issue in block and functional scope
